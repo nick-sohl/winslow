c193929c5c85e3aaa6f3b35d8377b8927a704e53
nick-sohl <nicksohl59@gmlail.com>
2025-09-03 23:42:18 +0200

    initialize project and create a basic folder structure

---
+++ src/main/java/ch/nicksohl/winslow/WinslowApplication.java
@@ -0,0 +1,13 @@
+package ch.nicksohl.winslow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinslowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinslowApplication.class, args);
+	}
+
+}
